1de237c6ad86cfd5927ce02ee54ac57924b18179
Andrey Galchevski <agalchevski@heed-global.com>
2018-12-23 11:57:54 +0200

    add create express generator structure

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 
+const createError = require('http-errors');
 const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const dbConfig = require('./config/db');
 const { getRawStats, getMonthlyAverages } = require('./routes/index');
@@ -8,13 +12,28 @@ const scheduler = require('./scheduler/index');
 
 const app = express();
 
-const port = process.env.PORT || 3000;
-
 dbConfig.connectToDB();
 scheduler.scheduleJob();
 
-app.use(express.static('public'));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/api/stats', getRawStats);
 app.get('/api/stats/:year/:country', getMonthlyAverages);
 
-app.listen(port, () => console.log(`Server is live on port ${port}`));
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
