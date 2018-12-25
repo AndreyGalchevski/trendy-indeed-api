5430be39ceab6fb85ce0a9d5b0111901e55053ae
Andrey Galchevski <agalchevski@heed-global.com>
2018-12-25 06:58:40 +0200

    add cors, remove client side delivery through public folder

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -15,11 +15,11 @@ const app = express();
 dbConfig.connectToDB();
 scheduler.scheduleJob();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/stats', getRawStats);
 app.get('/api/stats/:year/:country', getMonthlyAverages);
